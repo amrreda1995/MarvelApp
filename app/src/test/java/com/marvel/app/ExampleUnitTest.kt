68d4225522753cb27b2d599fa868c274f698d781
amr <amr.reda151@gmail.com>
2019-07-28 19:21:55 +0200

    refactor

---
+++ app/src/test/java/com/marvel/app/ExampleUnitTest.kt
@@ -0,0 +1,14 @@
+package com.marvel.app
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+
+class ExampleUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
